b34cdd9cf2f5bc044fe6a21a1656e09242895150
Whogoesthere <richardejtalcott@gmail.com>
2026-07-17 10:37:23 -0500

    Complete splash layout imports

--- app/src/main/java/com/richardtalcott/periodic/SplashActivity.kt
+++ app/src/main/java/com/richardtalcott/periodic/SplashActivity.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp\nimport androidx.compose.ui.unit.sp
 import kotlin.math.*
 
 class SplashActivity : ComponentActivity() {
