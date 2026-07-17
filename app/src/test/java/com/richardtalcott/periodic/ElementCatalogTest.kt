5783b1fdefe0093b18ebecc0169485cf7de70101
Whogoesthere <richardejtalcott@gmail.com>
2026-07-17 09:59:33 -0500

    Fix property catalog test formatting

--- app/src/test/java/com/richardtalcott/periodic/ElementCatalogTest.kt
+++ app/src/test/java/com/richardtalcott/periodic/ElementCatalogTest.kt
@@ -12,7 +12,13 @@ class ElementCatalogTest {
     assertEquals("Oganesson", ELEMENT_NAMES.last())
   }
 
-  @Test fun propertyCatalogCoversEveryElement() {\n    assertEquals(118, ELEMENT_PROPERTIES.size)\n    assertEquals("65.38(2)", ELEMENT_PROPERTIES[29].atomicMass)\n    assertEquals("[Ar] 3d10 4s2", ELEMENT_PROPERTIES[29].configuration)\n  }\n\n  @Test fun zincRegressionIsCorrect() {
+  @Test fun propertyCatalogCoversEveryElement() {
+    assertEquals(118, ELEMENT_PROPERTIES.size)
+    assertEquals("65.38(2)", ELEMENT_PROPERTIES[29].atomicMass)
+    assertEquals("[Ar] 3d10 4s2", ELEMENT_PROPERTIES[29].configuration)
+  }
+
+  @Test fun zincRegressionIsCorrect() {
     assertEquals("Zinc", ELEMENT_NAMES[29])
     assertEquals(65, REPRESENTATIVE_MASS_NUMBERS[29])
   }
