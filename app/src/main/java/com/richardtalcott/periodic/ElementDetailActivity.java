d5ecd35e63289b753c647d1a596ea77de7037714
Whogoesthere <richardejtalcott@gmail.com>
2026-07-17 02:19:58 -0500

    Enable uninterrupted immersive science display

--- app/src/main/java/com/richardtalcott/periodic/ElementDetailActivity.java
+++ app/src/main/java/com/richardtalcott/periodic/ElementDetailActivity.java
@@ -11,6 +11,7 @@ public final class ElementDetailActivity extends Activity {
         super.onCreate(b);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         int n=getIntent().getIntExtra("atomicNumber",8);
         FrameLayout root=new FrameLayout(this);
         gl=new True3DView(this,true,n);
