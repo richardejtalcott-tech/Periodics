75ef65c79b4974fc1b5496e65bb9251ee1c2a994
Whogoesthere <richardejtalcott@gmail.com>
2026-07-17 02:19:53 -0500

    Enable uninterrupted immersive science display

--- app/src/main/java/com/richardtalcott/periodic/MainActivity.java
+++ app/src/main/java/com/richardtalcott/periodic/MainActivity.java
@@ -12,6 +12,7 @@ public final class MainActivity extends Activity {
         super.onCreate(b);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         FrameLayout root=new FrameLayout(this);
         gl=new True3DView(this,false,0);
         TableHudView hud=new TableHudView(this,gl.renderer());
