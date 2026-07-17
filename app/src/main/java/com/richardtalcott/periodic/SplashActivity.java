22e79de9303711ddc2154515c570e9b5f323aa54
Whogoesthere <richardejtalcott@gmail.com>
2026-07-17 02:19:52 -0500

    Enable uninterrupted immersive science display

--- app/src/main/java/com/richardtalcott/periodic/SplashActivity.java
+++ app/src/main/java/com/richardtalcott/periodic/SplashActivity.java
@@ -12,6 +12,7 @@ public final class SplashActivity extends Activity {
         super.onCreate(state);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         setContentView(new AtomSplashView(this));
         handler.postDelayed(() -> { startActivity(new Intent(this, MainActivity.class)); finish(); }, 2800);
     }
