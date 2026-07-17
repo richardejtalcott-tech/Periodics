3bc067837069efb891f1d46ffa088ee02c1520cd
Whogoesthere <richardejtalcott@gmail.com>
2026-07-17 09:37:35 -0500

    Add complete 118-element identity and representative isotope data

---
+++ app/src/main/java/com/richardtalcott/periodic/ElementCatalog.kt
@@ -0,0 +1,13 @@
+package com.richardtalcott.periodic
+
+internal val ELEMENT_NAMES = listOf("Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon","Nitrogen","Oxygen","Fluorine","Neon","Sodium","Magnesium","Aluminium","Silicon","Phosphorus","Sulfur","Chlorine","Argon","Potassium","Calcium","Scandium","Titanium","Vanadium","Chromium","Manganese","Iron","Cobalt","Nickel","Copper","Zinc","Gallium","Germanium","Arsenic","Selenium","Bromine","Krypton","Rubidium","Strontium","Yttrium","Zirconium","Niobium","Molybdenum","Technetium","Ruthenium","Rhodium","Palladium","Silver","Cadmium","Indium","Tin","Antimony","Tellurium","Iodine","Xenon","Caesium","Barium","Lanthanum","Cerium","Praseodymium","Neodymium","Promethium","Samarium","Europium","Gadolinium","Terbium","Dysprosium","Holmium","Erbium","Thulium","Ytterbium","Lutetium","Hafnium","Tantalum","Tungsten","Rhenium","Osmium","Iridium","Platinum","Gold","Mercury","Thallium","Lead","Bismuth","Polonium","Astatine","Radon","Francium","Radium","Actinium","Thorium","Protactinium","Uranium","Neptunium","Plutonium","Americium","Curium","Berkelium","Californium","Einsteinium","Fermium","Mendelevium","Nobelium","Lawrencium","Rutherfordium","Dubnium","Seaborgium","Bohrium","Hassium","Meitnerium","Darmstadtium","Roentgenium","Copernicium","Nihonium","Flerovium","Moscovium","Livermorium","Tennessine","Oganesson")
+
+internal val REPRESENTATIVE_MASS_NUMBERS = intArrayOf(
+  1,4,7,9,11,12,14,16,19,20,23,24,27,28,31,32,35,40,39,40,
+  45,48,51,52,55,56,59,59,64,65,70,73,75,79,80,84,85,88,89,91,
+  93,96,98,101,103,106,108,112,115,119,122,128,127,131,133,137,
+  139,140,141,144,145,150,152,157,159,163,165,167,169,173,175,178,
+  181,184,186,190,192,195,197,201,204,207,209,209,210,222,223,226,
+  227,232,231,238,237,244,243,247,247,251,252,257,258,259,266,267,
+  268,269,270,269,278,281,282,285,286,289,290,293,294,294
+)
