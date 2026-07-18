dc2209da9c226487477c657e81551bc1301f6f6f
Whogoesthere <richardejtalcott@gmail.com>
2026-07-18 10:27:38 -0500

    Add first React Native element data set

---
+++ react-native/elements1.js
@@ -0,0 +1,2 @@
+export const SYMBOLS1='H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn'.split(' ');
+export const NAMES1='Hydrogen|Helium|Lithium|Beryllium|Boron|Carbon|Nitrogen|Oxygen|Fluorine|Neon|Sodium|Magnesium|Aluminum|Silicon|Phosphorus|Sulfur|Chlorine|Argon|Potassium|Calcium|Scandium|Titanium|Vanadium|Chromium|Manganese|Iron|Cobalt|Nickel|Copper|Zinc'.split('|');
