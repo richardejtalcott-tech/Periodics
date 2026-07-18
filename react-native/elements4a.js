486680aa3a37b7ec10f16be387927cb89db7a0f4
Whogoesthere <richardejtalcott@gmail.com>
2026-07-18 10:28:03 -0500

    Add fourth React Native element data set

---
+++ react-native/elements4a.js
@@ -0,0 +1,2 @@
+export const SYMBOLS4A='Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf'.split(' ');
+export const NAMES4A='Protactinium|Uranium|Neptunium|Plutonium|Americium|Curium|Berkelium|Californium|Einsteinium|Fermium|Mendelevium|Nobelium|Lawrencium|Rutherfordium'.split('|');
