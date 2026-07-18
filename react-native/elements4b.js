2c52d773414d93435de7a59a9a0e6cb03d01e9f0
Whogoesthere <richardejtalcott@gmail.com>
2026-07-18 10:28:12 -0500

    Complete React Native element data

---
+++ react-native/elements4b.js
@@ -0,0 +1,2 @@
+export const SYMBOLS4B='Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og'.split(' ');
+export const NAMES4B='Dubnium|Seaborgium|Bohrium|Hassium|Meitnerium|Darmstadtium|Roentgenium|Copernicium|Nihonium|Flerovium|Moscovium|Livermorium|Tennessine|Oganesson'.split('|');
